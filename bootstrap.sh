c8a26c651e2b4e41d9d585c1de7953d33123b5a1
Chris Rigby <squeeb@radioglitch.com>
2018-09-30 21:51:46 +0100

    Initital Commit

---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# vars
+reboot_required=false
+
+# functions
+function do_reboot {
+  if $reboot_required
+  then
+    osascript -e 'tell application "Finder" to restart'
+  fi
+}
+function swupdate {
+  if softwareupdate -l | grep -q "Update found"
+  then
+    echo "New software available. Installing updates."
+    sudo softwareupdate -ia && reboot_required=true
+  fi
+}
+function install_homebrew {
+  if [ ! $(which brew) ]
+  then
+     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+}
+
+# Update software
+swupdate
+
+# Install homebrew
+install_homebrew
+
+# Install ansible
+brew install ansible
+do_reboot
