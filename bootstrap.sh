7a999f61e84f803cb5ca6724d5339236c70d6b0e
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2021-06-07 08:42:03 +0100

    Brew bootstrap change. Package updates.
    - Alter brew bootstrap to use bash bootstrap script
    - Remove Cinch and Magnet, add Moom

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ function swupdate {
 function install_homebrew {
   if [ ! $(which brew) ]
   then
-     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 }
 
